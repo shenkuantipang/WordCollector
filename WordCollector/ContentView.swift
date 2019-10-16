eae1fd194adb807d16fcb79d5d73bb051371c8ce
oğulcan keskin <ogulcan.keskin@teb.com.tr>
2019-10-16 08:41:39 +0300

    Initial Commit

---
+++ WordCollector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordCollector
+//
+//  Created by ogulcan keskin on 16.10.2019.
+//  Copyright © 2019 ogulcan keskin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
